039b59a4abfcf0fb0854bfa51fde56f92e4c5f43
Lily Rithichoo <rithi22s@mtholyoke.edu>
2019-03-23 20:13:44 +0700

    Add problem 1 in Scala

--- scala/src/main/scala/ProjEulerP1.scala
+++ scala/src/main/scala/ProjEulerP1.scala
@@ -1,1 +1,9 @@
+class ProjEulerP1 {
+  val result = (1 until 1000).view.filter((x) => (x % 3 == 0 || x % 5 == 0)).sum
+}
 
+object solution extends ProjEulerP1 {
+  def main(args: Array[String]): Unit = {
+    print(result)
+  }
+}
